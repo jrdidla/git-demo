acdc7074849796c9a7cee31ffc93581dd41200fb
unknown <jrdidla@gmail.com>
2023-09-30 22:52:05 -0400

    new method added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,14 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		HelloWorld hl = new HelloWorld();
+		hl.add();
+		
 
+	}
+	
+	public void add() {
+		System.out.println("This is test");
 	}
 
 }
